c2d4c0e7f0c87db8fb215bc209f6f1d6a5dee276
deborasilvaaaa <deborasilvacardosos021@gmail.com>
2023-10-11 11:46:45 -0300

    adicionei atividade

---
+++ chaidissonatividade5.py
@@ -0,0 +1,7 @@
+# Digite uma metragem e veja isso em cm e mm:
+print('descubra a metragem de um número  em centimetros e milimetros')
+numero = float(input('digite um número: '))
+centimetro = numero/ 100
+milimetro = numero/1000
+print(f' o numero é {centimetro}cm e o número é {milimetro}mm ')
+
